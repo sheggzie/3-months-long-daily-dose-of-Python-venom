26185f2fe2bc3fa0bbae066e6ae0fd6ebe8870f6
quadrey-sheggzie <akanniquadrey@gmail.com>
2024-06-25 00:52:53 +0100

    initial commit

---
+++ WEEK 1/d2t1.py
@@ -0,0 +1,4 @@
+# Create a script that prints "Hello" 10 times.
+
+for i in range(10):
+    print(i + 1, "Hello")
---
+++ WEEK 1/d2t2.py
@@ -0,0 +1,8 @@
+# Write a program that finds the largest number in a list.
+
+list_one = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+def largest(list):
+    print(f"{max(list)} is the largest number in the list")
+
+largest(list_one)
