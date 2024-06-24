d43c9da73eb165a3d6535eace29bd9750dd951b2
quadrey-sheggzie <akanniquadrey@gmail.com>
2024-06-25 00:31:57 +0100

    added comments

--- WEEK 1/d1t1.py
+++ WEEK 1/d1t1.py
@@ -1,6 +1,7 @@
 # Create a script to calculate the area of a circle given its radius.
 
 def area(radius):
+    # Formular to get Area of a circle is "a = pi(3.14) * (power of r(radius))"
     a = 3.14 * pow(radius, 2)
     return int(a)
 
---
+++ WEEK 1/d1t2.py
@@ -0,0 +1,2 @@
+# Write a program that converts Celsius to Fahrenheit.
+
