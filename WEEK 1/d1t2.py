7ce0a67966d2e665077b97111f1734d8d39de361
quadrey-sheggzie <akanniquadrey@gmail.com>
2024-06-25 00:40:14 +0100

    initial commit

--- WEEK 1/d1t2.py
+++ WEEK 1/d1t2.py
@@ -1,2 +1,9 @@
 # Write a program that converts Celsius to Fahrenheit.
 
+# Formular for Fahrenheit is F = C * (9/5) + 32
+
+def convert_temp(celcius):
+    f = celcius * (9/5) + 32
+    return f
+
+print(int(convert_temp(100)))
