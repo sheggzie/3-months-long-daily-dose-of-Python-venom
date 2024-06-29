a5f6a42e1c683920bd430871165a0ba0e9d3b168
quadrey-sheggzie <akanniquadrey@gmail.com>
2024-06-29 13:28:54 +0100

    optimized
    - enclosed it in a function
    - included a parameter that takes the file name to be read
    - Added comments for better readability and clarity.

--- WEEK 1/d5t1.py
+++ WEEK 1/d5t1.py
@@ -1,10 +1,15 @@
 # Create a script to read a text file and count the number of lines.
 
-with open("random.txt", "r") as file:
-    lines = file.readlines()
-    counts = 0
+# Function to read the count of lines, takes the document name to be read as parameter
+def count_lines(doc):
+    # Open file as a readable format
+    with open(doc, "r") as file:
+        # Read each line in the file with the readlines() function
+        lines = file.readlines()
+        line_count = 0
 
-    for line in lines:
-        counts += 1
-
-print(counts)
+        # Increment the value of variable "line_count" for each line read
+        for line in lines:
+            line_count += 1
+        print(line_count)
+count_lines("random.txt")
