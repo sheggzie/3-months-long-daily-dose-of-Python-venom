444dc16f435cf15417d67a862c0b74ef9a3d3ac2
quadrey-sheggzie <akanniquadrey@gmail.com>
2024-07-03 17:18:18 +0100

    Optimized code
    - added a guide to help navigate the CLI.
    
    - added the 'clear_tasks()' function to clear the whole contents of the task file.
    
    - Changed the conditional logic of the program loop from 'if statements' to 'match' for better functionality.
    
    - Added 'try-except' method to catch errors and run program smoothly to some parts of the code to make it run error free.

--- WEEK 1/d6and7.py
+++ WEEK 1/d6and7.py
@@ -4,11 +4,22 @@
 # Add a task
 # Remove a task
 # List all tasks
+# Clear saved tasks
 # Store tasks in a file so they persist between program runs.
 
-line_number = 0
 lines = ''
-def tasker():    
+def tasker():
+    print("Hello! Welcome to your CLI task manager.")
+    print("Below are some helpful tips you might need!")
+    print("To add a task, just enter the task e.g 'Make coffee after exercise'")
+    print("To see your previously added available tasks, simply type 'show tasks'")
+    print("To delete a task from list, enter 'remove' or 'delete', then enter the task number/ID to delete it")
+    print("To clear the whole contents of a task, simply type 'clear tasks'")
+    print("To close or exit once you're done, simply enter 'end' or 'exit' or 'close' or 'stop' or 'quit'")
+    print("Have fun tasking :)")
+    print("Loading...")
+    print("Starting...")
+    print("Shoot!!!")    
     def add_task(new_task, filename="task.txt"):
         
         with open(filename, "r") as file:
@@ -17,17 +28,15 @@ def tasker():
                         
         
         with open(filename, "a") as file:
-            global line_number
-            line_number = 1
-            for i in lines:
-                line_number += 1
-            file.write(str(line_number) + ": " + new_task + "\n")
+            file.write(new_task + "\n")
+        
+        print(f"New task '{new_task}' has been added to your task list")
 
     def remove_task(item):  
         try:      
             with open("task.txt", "r") as file:
                 lines = file.readlines()
-                
+
             if item < 1:
                 print(f"{item} is an invalid number, enter a number greater than or equal to 1")
             elif item > len(lines):
@@ -48,18 +57,35 @@ def tasker():
         with open("task.txt", "r") as file:
             print(file.readlines())
 
-    end_prompt = ["end", "close", "stop", "exit", "quit"]    
-    while True:    
-        prompt = input("What would you like to do?: ").lower()
-        if prompt.lower() in end_prompt:
-            print("Ending loop...")
-            break
-        elif prompt == "show tasks":
-            list_tasks()
-        elif prompt == "remove" or "Remove" or "delete" or "Delete":
-            ask = int(input("Enter the line number to be deleted: "))
-            remove_task(ask)
-        else:
-            add_task(prompt)
-            print(prompt)
+    def clear_tasks():
+        with open("task.txt", "w") as file:
+            file.truncate()
+    
+    while True:        
+        prompt = input("What would you like to do?: ").lower()              
+        match prompt:
+            case "end" | "close" | "stop" | "exit" | "quit":
+                print("Closing...")
+                break
+            case "show tasks":
+                list_tasks()
+            case "remove" | "delete":
+                try:
+                    ask = int(input("Enter the line number to be deleted: "))
+                    remove_task(ask)
+                except Exception as e:
+                    print(f"Oops! There was an error '{e}'")
+            case "clear tasks":
+                try:
+                    confirmation = str(input("Are you sure you want to clear all saved tasks? y/n: "))
+                    if confirmation == "y":
+                        clear_tasks()
+                        print("All tasks have been cleared successfully!")
+                        break
+                    elif confirmation == "n":
+                        break
+                except Exception as e:
+                    print(f"Oops! An error '{e}' occured")
+            case _:
+                add_task(prompt)
 tasker()
