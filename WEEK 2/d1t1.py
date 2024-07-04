7a8e484d6224ada3d52de5b3be73d34da1c9c163
quadrey-sheggzie <akanniquadrey@gmail.com>
2024-07-04 13:00:47 +0100

    Day 1 tasks done

---
+++ WEEK 2/d1t1.py
@@ -0,0 +1,17 @@
+# Create a list of numbers and perform various operations: add, remove, sort, reverse.
+
+number_list = [11, 25, 2, 18, 4, 6, 12, 9, 8, 5]
+
+# add number to list
+# number_list.append(7)
+
+# remove number from list
+# number_list.remove(6)
+
+# sort list
+# number_list.sort()
+
+# reverse list
+# number_list.reverse()
+
+print(number_list)
---
+++ WEEK 2/d1t2.py
@@ -0,0 +1,8 @@
+# Create a tuple with different data types and access its elements.
+
+my_tuple = ("a", False, 1, "23", "sheggzie", True, "Tuple")
+other_tuple = (["26", "19", 16], [18], True, {"name": "Sheggzie"}, 30, False)
+
+print(my_tuple[2])
+print(other_tuple[0][2])
+print(other_tuple[3]["name"])
