2f89b4a583b5556e1cb059d7f51de0fe258cdf2d
quadrey-sheggzie <akanniquadrey@gmail.com>
2024-07-05 17:01:54 +0100

    d2t1 done

---
+++ WEEK 2/d2t1.py
@@ -0,0 +1,41 @@
+# Create a dictionary of student names and their scores, and perform various operations.
+
+# create dictionary
+students = {"daniel" : 96,
+           "wisdom": 95,
+           "gbenga": 95,
+           "quadri": 94,
+           "precious": 94,
+           "samuel": 94,
+           "kehinde": 94
+           }
+
+# add item to dictionary
+# students.update({"sheggzie": 99})
+
+# remove item from dictionary
+# students.pop("quadri") # takes the key value as argument
+# students.popitem() # takes no argument, pops or removes the last item in the dictionary
+# del students["quadri"] # deletes item referenced in the key from the dictionary
+
+
+# duplicate dictionary
+# backup = students.copy()
+# print(backup)
+
+# clears dictionary items
+# students.clear()
+
+# get keys, values and items
+# print(students.keys())
+# print(students.values())
+# print(students.items())
+
+# iterate over dictionary
+# for key in students:
+#     print(key, students[key])
+
+# for key, value in students.items():
+#     print(key, value)
+
+# print(students)
