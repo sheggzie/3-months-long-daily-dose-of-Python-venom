89b0fc2c3fe4ab9ba280096c111a2bfe3886c985
quadrey-sheggzie <akanniquadrey@gmail.com>
2024-07-05 17:19:42 +0100

    d2t2 done

---
+++ WEEK 2/d2t2.py
@@ -0,0 +1,23 @@
+# Create a set of unique words from a list of words and perform set operations.
+
+# create set
+my_set = {1, 2, 3, 4, 5}
+other_set = {6, 7, 8, 9, 10}
+new_set = {2, 4, 6, 8}
+
+# add to set
+# my_set.add(6)
+# print(my_set)
+
+# remove from set
+# my_set.remove(5)
+# print(my_set)
+
+# union set operation
+# print(my_set.union(other_set))
+
+# intersection in set operation
+# print(my_set.intersection(new_set))
+
+# difference in set operation
+# print(my_set.difference(new_set))
