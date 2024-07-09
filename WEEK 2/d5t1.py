97b0efe58c498cc45842ecb4972fdf55eba2140e
quadrey-sheggzie <akanniquadrey@gmail.com>
2024-07-09 17:26:53 +0100

    optimized
    - prompt asks for students name and scores and writes it to "records.csv"
    
    - entering the "x" in prompt exits the program

--- WEEK 2/d5t1.py
+++ WEEK 2/d5t1.py
@@ -1,5 +1,36 @@
 # Create a script that reads a file of student names and scores, stores them in a dictionary, and performs various operations.
+import csv
 
-def students(filename="records.txt"):
+students = {}
+filename = "records.csv"
+
+def records(filename):
     with open(filename, "r") as file:
-        file.readlines()
+        lines = csv.reader(file)
+        
+        for name, scores in lines:
+            students[name] = int(scores)
+
+def add_record(name, score):
+    students[name] = int(score)
+    print(f"Student name: {name} with score: {score} added to students")
+    print(students)   
+
+def save_records(filename, students):
+    with open(filename, "w", newline="") as file:
+        writer = csv.writer(file)
+        for name, score in students.items():
+            writer.writerow([name, score])
+
+
+while True:
+    print("Enter 1 to add record")
+    print("Enter x to exit")
+    prompt = input("What do you want to do? ")
+    if prompt == "1":
+        name = input("Enter student name: ")
+        score = input("Enter student score: ")
+        add_record(name, score)
+    elif prompt == "x":
+        save_records(filename, students)
+        break
