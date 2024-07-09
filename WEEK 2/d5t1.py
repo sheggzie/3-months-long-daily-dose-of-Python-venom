3b68c7c9a866ca9eb886503024de2cda0922850a
quadrey-sheggzie <akanniquadrey@gmail.com>
2024-07-09 18:13:36 +0100

    - added the "remove_student()" function to delete a student record if needed.

--- WEEK 2/d5t1.py
+++ WEEK 2/d5t1.py
@@ -2,19 +2,29 @@
 import csv
 
 students = {}
-filename = "records.csv"
+filename = "student records.txt"
 
 def records(filename):
+    global students
     with open(filename, "r") as file:
         lines = csv.reader(file)
         
         for name, scores in lines:
             students[name] = int(scores)
+    return students
 
 def add_record(name, score):
+    global students
     students[name] = int(score)
     print(f"Student name: {name} with score: {score} added to students")
     print(students)   
+
+def remove_student(students, name):
+    if name in students:
+        del students[name]
+        print(f"Student with name: {name} removed.")
+    else:
+        print("Students record not found!")
 
 def save_records(filename, students):
     with open(filename, "w", newline="") as file:
@@ -23,14 +33,23 @@ def save_records(filename, students):
             writer.writerow([name, score])
 
 
-while True:
-    print("Enter 1 to add record")
-    print("Enter x to exit")
-    prompt = input("What do you want to do? ")
-    if prompt == "1":
-        name = input("Enter student name: ")
-        score = input("Enter student score: ")
-        add_record(name, score)
-    elif prompt == "x":
-        save_records(filename, students)
-        break
+
+def main():
+    students = records(filename)
+    while True:
+        print("Enter 1 to add record.")
+        print("Enter 2 to remove a student record.")
+        print("Enter x to exit.")
+        prompt = input("What do you want to do? ")
+        if prompt == "1":
+            name = input("Enter student name: ")
+            score = input("Enter student score: ")
+            add_record(name, score)
+        elif prompt == "2":
+            ask = input("Enter name of Student to remove: ")
+            remove_student(students, ask)
+        elif prompt == "x":
+            save_records(filename, students)
+            break
+
+main()
