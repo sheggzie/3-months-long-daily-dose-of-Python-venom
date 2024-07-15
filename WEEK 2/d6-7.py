cbf297a5f9079c71bbd5ee190488690011667df7
quadrey-sheggzie <akanniquadrey@gmail.com>
2024-07-15 21:32:32 +0100

    initial commit
    - created a framework for the program functions

---
+++ WEEK 2/d6-7.py
@@ -0,0 +1,18 @@
+# Book Catalog Project:
+# Create a command-line interface (CLI) book catalog.
+# Features to implement:
+# Add a book (title, author, year)
+# Remove a book
+# Search for a book by title or author
+# List all books
+# Store books in a file so they persist between program runs.
+
+def booker():
+    def add_book():
+        pass
+
+    def remove_book():
+        pass
+
+    def list_books():
+        pass
