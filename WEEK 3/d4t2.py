323703a471f9dce5157924c1ad699f4e1143e0a2
quadrey-sheggzie <akanniquadrey@gmail.com>
2024-07-19 23:45:59 +0100

    polymorphism done

--- WEEK 3/d4t2.py
+++ WEEK 3/d4t2.py
@@ -1,6 +1,6 @@
 # Write a program that demonstrates method overriding and polymorphism.
 
-# METHOD OVERRIDING
+# 1. METHOD OVERRIDING
 
 # declare class Horse
 class Horse:
@@ -15,5 +15,30 @@ class Cheetah(Horse):
 fast = Horse()
 faster = Cheetah()
 
-fast.run()
-faster.run() # Overrides the inherited run output and prints "I can run faster" 
+# fast.run()
+# faster.run() # Overrides the inherited run output and prints "I can run faster" 
+
+
+
+
+# 2. POLYMORPHISM
+
+# declare class Horse
+class Horse:
+    def run(self):
+        print("I can run.")
+
+# declare class Cheetah which inherits from Horse
+class Cheetah(Horse):
+    def run(self):
+        print("I can run fasterrrrrrrr!")
+
+# Polymorphing function that outputs based on class passed as parameter
+def runner(animal):
+    animal.run()
+
+horse = Horse()
+cheetah = Cheetah()
+
+runner(horse)
+runner(cheetah)
