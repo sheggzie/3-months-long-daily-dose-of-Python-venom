1f7f02b15d476f4e8a3dbecef69bc1284add8d0e
quadrey-sheggzie <akanniquadrey@gmail.com>
2024-07-30 20:10:59 +0100

    d1t1 done

--- WEEK 4/d1t1.py
+++ WEEK 4/d1t1.py
@@ -1,1 +1,15 @@
 # Use the requests library to fetch data from a URL.
+
+import requests
+
+# get bitcoin price data from coindesk api
+def fetcher():
+    url = "https://api.coindesk.com/v1/bpi/currentprice.json"
+    try:
+        response = requests.get(url)
+        data = response.json()          # convert fetched data to json file
+        print(f"Bitcoin data: {data}")
+    except Exception as e:
+        print(f"Error! An error {e} was encountered.")
+  
+fetcher()
